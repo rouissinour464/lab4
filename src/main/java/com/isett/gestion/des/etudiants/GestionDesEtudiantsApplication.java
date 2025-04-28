8e2e5ca3d12f44330aafb676ddd2ce0f14a2559b
nour <nourrouissi374@gmail.com>
2025-04-28 20:30:15 +0200

    first commit

---
+++ src/main/java/com/isett/gestion/des/etudiants/GestionDesEtudiantsApplication.java
@@ -0,0 +1,13 @@
+package com.isett.gestion.des.etudiants;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GestionDesEtudiantsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GestionDesEtudiantsApplication.class, args);
+	}
+
+}
